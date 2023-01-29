739fb1faab65f1969492e415f4d9ad77ff2194c0
Sigroy <sigfredoolmedo6c17@gmail.com>
2023-01-29 17:06:17 -0600

    test advanced routing

--- public/index.php
+++ public/index.php
@@ -10,15 +10,21 @@ $router = new Router();
 // Add the routes
 $router->add('', ['controller' => 'Home', 'action' => 'index']);
 $router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
-$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
+//$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
+$router->add('{controller}/{action}');
+$router->add('admin/{action}/{controller}');
+$router->add('{controller}/{id:\d+}/{action}');
 
 // Display the routines table
-//echo '<pre>';
+echo '<pre>';
 //var_dump($router->getRoutes());
-//echo '</pre>';
+echo htmlspecialchars(print_r($router->getRoutes(), true));
+echo '</pre>';
 
 // Match the requested route
 $url = $_SERVER['QUERY_STRING'];
+
+// string(9) "posts/new"
 
 if ($router->match($url)) {
     echo '<pre>';
