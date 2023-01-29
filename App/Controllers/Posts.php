420f9dffc9f2bec8fdf3b7a348bb3234fa2241ba
Sigroy <sigfredoolmedo6c17@gmail.com>
2023-01-29 17:58:36 -0600

    add Posts controller to test the router

---
+++ App/Controllers/Posts.php
@@ -0,0 +1,24 @@
+<?php
+
+class Posts
+{
+    /**
+     * Show the index page
+     *
+     * @return void
+     */
+    public function index(): void
+    {
+        echo 'Hello from the index action in the Posts controller!';
+    }
+
+    /**
+     * Show the add new page
+     *
+     * @return void
+     */
+    public function addNew(): void
+    {
+        echo 'Hello from the addNew action in the Posts controller!';
+    }
+}
