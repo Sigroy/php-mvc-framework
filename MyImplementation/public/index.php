da7d06950ad45a8aa2b8c0d508b3b7808c8cc64c
Sigroy <sigfredoolmedo6c17@gmail.com>
2023-01-30 00:35:00 -0600

    Check adding routers and dispatching the query string

--- MyImplementation/public/index.php
+++ MyImplementation/public/index.php
@@ -1,1 +1,12 @@
 <?php
+
+require '../Core/Router.php';
+
+use MyImplementation\Core\Router;
+
+$router = new Router();
+
+$router->add('{controller}/{action}');
+$router->add('{controller}/{id:\d+}/{action}');
+
+$router->dispatch($_SERVER['QUERY_STRING']);
