ab1ea909c691573d9c6c102c3133d4d86abebeb2
Sigroy <sigfredoolmedo6c17@gmail.com>
2023-02-04 22:46:04 -0600

    Add methods to the posts controller

--- App/Controllers/Posts.php
+++ App/Controllers/Posts.php
@@ -1,15 +1,18 @@
 <?php
 
-class Posts
+namespace MVCFramework\App\Controllers;
+class Posts extends \MVCFramework\Core\Controller
 {
     /**
      * Show the index page
      *
      * @return void
      */
-    public function index(): void
+    public function indexAction(): void
     {
         echo 'Hello from the index action in the Posts controller!';
+        echo '<p>Query string parameters: <pre>' .
+            htmlspecialchars(print_r($_GET, true)) . '</pre></p>';
     }
 
     /**
@@ -17,8 +20,20 @@ class Posts
      *
      * @return void
      */
-    public function addNew(): void
+    public function addNewAction(): void
     {
         echo 'Hello from the addNew action in the Posts controller!';
+    }
+
+    /**
+     * Show the edit page
+     *
+     * @return void
+     */
+    public function editAction()
+    {
+        echo 'Hello from the edit action in the Posts controller!';
+        echo '<p>Route parameters: <pre>' .
+            htmlspecialchars(print_r($this->route_params, true)) . '</pre></p>';
     }
 }
