358b0a087547f27522e67a55b65bf1041e85890b
Sigroy <sigfredoolmedo6c17@gmail.com>
2023-02-04 22:47:09 -0600

    Create an abstract class for a base controller

---
+++ Core/Controller.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace MVCFramework\Core;
+
+abstract class Controller
+{
+    /**
+     * Parameters from the matched route
+     * @var array
+     */
+    protected array $route_params = [];
+
+    /**
+     * Class constructor
+     *
+     * @param array $route_params Parameters from the route
+     *
+     * @return void
+     */
+    public function __construct(array $route_params)
+    {
+        $this->route_params = $route_params;
+    }
+
+    /**
+     *
+     * @param string $name
+     * @param array $arguments
+     * @return void
+     */
+    public function __call(string $name, array $arguments): void
+    {
+        $method = $name . 'Action';
+
+        if (method_exists($this, $method)) {
+            if ($this->before() !== false) {
+                call_user_func_array([$this, $method], $arguments);
+                $this->after();
+            }
+        } else {
+            echo 'Method ' . str_replace('Action', '', $method) . ' not found in controller ' . get_class($this);
+        }
+    }
+
+    /**
+     * Before filter - called before an action method.
+     *
+     * @return mixed
+     */
+    protected function before(): mixed
+    {
+
+    }
+
+    /**
+     * After filter - called after an action method.
+     *
+     * @return void
+     */
+    protected function after(): void
+    {
+
+    }
+}
