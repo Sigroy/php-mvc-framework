2177420cb288e823c257604a710640fc06548f33
Sigroy <sigfredoolmedo6c17@gmail.com>
2023-02-09 17:49:23 -0600

    Add a class for all the configuration constants

---
+++ App/Config.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace MVCFramework\App;
+
+class Config
+{
+
+    const DB_HOST = 'localhost';
+
+    const DB_NAME = 'mvc_framework';
+
+    const DB_USER = 'sig';
+
+    const DB_PASSWORD = '1234';
+
+    const SHOW_ERRORS = true;
+
+}
